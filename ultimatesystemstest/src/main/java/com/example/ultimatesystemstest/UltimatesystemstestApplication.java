03a8fd9204327150d3a192d4be58232bf5a251c9
Half0man <gregor0ad@gmail.com>
2022-11-22 19:40:10 +0100

    initial commit

---
+++ ultimatesystemstest/src/main/java/com/example/ultimatesystemstest/UltimatesystemstestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ultimatesystemstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimatesystemstestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UltimatesystemstestApplication.class, args);
+    }
+
+}
---
+++ ultimatesystemstest/src/test/java/com/example/ultimatesystemstest/UltimatesystemstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ultimatesystemstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimatesystemstestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
